46c1a5aa0ea58cee581f60e70ebaabd65570ba47
Gerald Bauer <gerald.bauer@gmail.com>
2024-10-03 13:52:48 +0200

    up

--- backup/backup.rb
+++ backup/backup.rb
@@ -6,6 +6,7 @@ reposet = GitRepoSet.read( './repos.yml' )
 #=>  556 repo(s) @ 69 org(s)  - on Oct/11 2023
 #=>  556 repo(s) @ 70 org(s)  - on Jan/28 2024   ~ about 2 GiG
 #=>  556 repo(s) @ 69 org(s)  - on March/3 2024
+#=>  552 repo(s) @ 71 org(s)  - on Oct/3 2024
 
 backup = GitBackup.new( '/backup2024' )
 backup.backup( reposet )
